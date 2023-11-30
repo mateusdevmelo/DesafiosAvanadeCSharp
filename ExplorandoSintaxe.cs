3d1d556a69f0157a4884e5fc10d2f08bb6b5bb24
Mateus Melo <mateusdevmelo@gmail.com>
2023-11-29 22:42:26 -0300

    first commit

---
+++ ExplorandoSintaxe.cs
@@ -0,0 +1,15 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    string email = Console.ReadLine();
+    string nome = Console.ReadLine();
+    string senha = Console.ReadLine();
+
+    Console.WriteLine($"{nome}, verifique o email: {email} para ativar.");
+
+
+    }
+}
+    
